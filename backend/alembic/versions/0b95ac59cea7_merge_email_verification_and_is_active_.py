92ab22a0492ba06faa4c4a3c8f08c7b1a7a2818c
userrmalak <159655472+userrmalak@users.noreply.github.com>
2026-08-01 22:58:07 +0300

    db: merge email_verification and is_active migration heads

---
+++ backend/alembic/versions/0b95ac59cea7_merge_email_verification_and_is_active_.py
@@ -0,0 +1,28 @@
+"""merge email verification and is_active heads
+
+Revision ID: 0b95ac59cea7
+Revises: c1a9e4f7d382
+Create Date: 2026-08-01 22:52:46.987487
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '0b95ac59cea7'
+down_revision: Union[str, Sequence[str], None] = 'c1a9e4f7d382'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
